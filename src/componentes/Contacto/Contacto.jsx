01272fe7007d4888ff05abe8327f38f099a8f252
jesus troconiz <jtroconiz@outlook.es>
2022-08-28 17:02:34 -0300

    Mapa google

--- src/componentes/Contacto/Contacto.jsx
+++ src/componentes/Contacto/Contacto.jsx
@@ -17,9 +17,11 @@ return (
         <h5>Ubicacion</h5>
         <ul className="list-unstyled text-small">
           <li className="mb-1"><a Target="_blank" className="" href="https://g.page/big-food-palermo?share">Malabia 1893, CABA Argentina <p>Plaza Armenia</p></a></li>
-          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#">Futuro mapa</a></li>
+          <li className="mb-1"><a className="link-secondary text-decoration-none" href="#"> <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13138.388121519969!2d-58.4243136!3d-34.5890616!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x4b157027254e4c3f!2sBig%20Food!5e0!3m2!1ses!2sar!4v1661716748891!5m2!1ses!2sar" ></iframe></a></li>
+
          
         </ul>
+       
       </div>
       <div className="col-4 col-md">
         <h5>Contacto</h5>
