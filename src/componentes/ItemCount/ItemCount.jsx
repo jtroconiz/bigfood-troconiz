e918895e4f10e91de4b172fe7fc9f9c0e4280439
jesus troconiz <jtroconiz@outlook.es>
2022-09-03 15:59:07 -0300

    Boton

--- src/componentes/ItemCount/ItemCount.jsx
+++ src/componentes/ItemCount/ItemCount.jsx
@@ -1,18 +1,27 @@
 import React, {useState} from "react";
 import Item from "../ItemListContainer/ItemList/Item/Item";
 import ItemList from "../ItemListContainer/ItemList/ItemList";
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+
 
 import './ItemCount.css' 
 
 const ItemCount = ( props ) => {
 const [clicks, setClicks] = useState(1);
+const [itemStock, setItemStock] = useState(props.stock);
+const MySwal = withReactContent(Swal)
 
+// console.log(props.stock)
+console.log(itemStock)
     return(
+        
         <div className="ItemCount">
-            <div>
+           
+            <div className="ItemCoun">
                 <button className="btn btn-secundary" role="button"
                 onClick={ ( ) => { 
-                    if ( clicks < props.stock   ) {
+                    if ( clicks < itemStock   ) {
                         setClicks(clicks + 1);
                         }else 
                         setClicks (clicks)
@@ -28,8 +37,27 @@ const [clicks, setClicks] = useState(1);
                         setClicks (clicks)
                     }}>-</button>
              </div>
-          <button className="btn btn-bf">Agregar al carrito</button>   
-      
+                 <button className="btn btn-bf"  
+                onClick={ () => {
+                    if (clicks <= itemStock){
+                        setItemStock(itemStock - clicks);
+                        setClicks(1);
+                      
+                    }
+                    else {
+                        MySwal.fire({
+                            title: <p>Se agoto el Stock</p>,
+                            didOpen: () => {
+                              // Avso cuando se agota el stock
+                              MySwal.showLoading()
+                                
+                            },
+                          })
+                    }
+                    }}>Agregar al carrito    </button>   
+
+                    <div>Unidades disponibles = {itemStock}</div>
+        {/* <Item stockonline={itemStock}/> */}
         </div>
     );
 }
--- src/componentes/ItemListContainer/ItemList/Item/Item.jsx
+++ src/componentes/ItemListContainer/ItemList/Item/Item.jsx
@@ -2,12 +2,8 @@ import React from "react";
 import ItemCount from "../../../ItemCount/ItemCount";
 
 
-const Item = ( props ) => {
-    const { 
+const Item = ( {items} ) => {
 
-    items,
-
-} = props;
 
 
  return(
@@ -15,12 +11,29 @@ const Item = ( props ) => {
 
             
 {
-items.map( (elemento) => {
-         return   <li key={elemento.id} className="Menu" id="Menu">
-                <h4 className="TituloItem d-flex justify-content-between"> {elemento.nombre}    <ItemCount stock={elemento.stock}/> </h4>
-                <p className="PrecioItem"> {elemento.precio} Ars</p>
-                <p>Unidades disponibles: {elemento.stock}</p> 
-            </li>
+items.map( (items) => {
+         return   <li key={items.id} className="Menu" id="Menu">
+                      <div className=" mb-3" >
+                        <div className="row no-gutters">          
+                            <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">{items.nombre} </h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                <p className="text-muted"><small className="text-muted">{items.precio} Ars</small></p>
+                            </div>
+                            </div>
+                            <div className="col-md-4">
+                            <ItemCount stock={items.stock}/>
+                            </div>
+                        </div>
+                    </div>
+          {/* // <h4 className="TituloItem d-flex justify-content-between"> {items.nombre}    <ItemCount stock={items.stock}/> </h4>
+// <p className="PrecioItem"> {items.precio} Ars</p>
+// {/* <p>Unidades disponibles: {items.precio={ <ItemCount {items.precio}/> }</p>  
+/</li> */}
+          
+          
+            </li> 
 })
 }
 
@@ -114,3 +127,26 @@ export default Item;
    
    
    
+
+
+// return   <li key={items.id} className="Menu" id="Menu">
+
+
+// <div class="card mb-3" style="max-width: 540px;">
+// <div class="row no-gutters">
+//     <div class="col-md-4">
+//     <img src="..." class="card-img" alt="...">
+//     </div>
+//     <div class="col-md-8">
+//     <div class="card-body">
+//         <h5 class="card-title">Card title</h5>
+//         <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+//         <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+//     </div>
+//     </div>
+// </div>
+// </div>
+// <h4 className="TituloItem d-flex justify-content-between"> {items.nombre}    <ItemCount stock={items.stock}/> </h4>
+// <p className="PrecioItem"> {items.precio} Ars</p>
+// {/* <p>Unidades disponibles: {items.precio={ <ItemCount {items.precio}/> }</p>  */}
+// </li>
